d56147c28a19a42156b65058a535fdfa9f17e161
mathildemerry <mathildem.pedersen@gmail.com>
2021-05-27 13:02:02 +0200

    Implemented TextToSpeech

--- app/src/main/java/com/example/demoproject/MainActivity.kt
+++ app/src/main/java/com/example/demoproject/MainActivity.kt
@@ -5,19 +5,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import java.util.*
         // for the command line
         // adb emu avd hostmicon
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, TextToSpeech.OnInitListener {
 
     lateinit var button : Button
     lateinit var textView : TextView
     private val REQUEST_CODE_SPEECH_INPUT = 100
+
+    private var tts: TextToSpeech? = null //variable for text to speech
+            //nullable being initialized in onCreate
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +32,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button = findViewById(R.id.voice_btn)
         button.setOnClickListener(this)
         textView = findViewById(R.id.textV)
+
+        //for the TTS
+        tts = TextToSpeech(this,this) //initializing TTS
+        // main activity is both listener and context (everything is going on here)
+        var button2 : Button
+        button2 = findViewById(R.id.btnTTS)
+        var etEnteredText : EditText
+        etEnteredText = findViewById(R.id.editT)
+
+        button2.setOnClickListener{
+            if (etEnteredText.text.isEmpty()) {
+                Toast.makeText(this@MainActivity,"Enter a text", Toast.LENGTH_SHORT).show()
+            } else{
+                readOut(etEnteredText.text.toString())
+            }
+        }
+
+
     }
 
     override fun onClick (view: View?){
@@ -72,6 +97,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }
         }
+    }
+// text to speech
+    override fun onInit(status: Int) {
+            //if textToSpeech can be used: (status ok)
+         if (status == TextToSpeech.SUCCESS){
+                    //language is set
+                    val output = tts!!.setLanguage(Locale.US)
+
+                    if (output == TextToSpeech.LANG_MISSING_DATA || output == TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("TTS","The language is not supported")
+                    }
+
+                }
+
+            }
+    private fun readOut(text: String) {
+        tts!!.speak(text,TextToSpeech.QUEUE_FLUSH,null, 1.toString())
+
     }
 }
 
